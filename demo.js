3db5516a783989f74cf6b7285ac9d25ecb8b444c
pcrglennon <pcrglennon@gmail.com>
2014-09-10 17:04:03 -0400

    Re-do, renders 1 todo element woohooooooo

--- demo.js
+++ demo.js
@@ -7,42 +7,36 @@ var Todo = Backbone.Model.extend({
   },
 
   initialize: function() {
-    console.log("todo has been initialized");
-    this.on("change:title", function() {
-      console.log("title changed to > " + this.get("title"));
-    });
+
   }
 });
 
-// Instantiate the Todo Model with a title, with the completed attribute
-// defaulting to false
-var myTodo = new Todo({
-  title: 'Check attributes property of the logged models in the console.'
-});
+var myTodo = new Todo({title: 'Fix this junk'});
+
+// Views
+
 
 var TodoView = Backbone.View.extend({
 
-  tagName: 'ul',
-  id: 'todos',
+  tagName: 'li',
 
   // Cache the template function for a single item.
-  todoTpl: _.template( $('#item-template').html() ),
+  template: _.template( $('#item-template').html() ),
 
   // Called when the view is first created
   initialize: function() {
     this.model.on('change', this.render, this);
+
+    this.render();
   },
 
   // Re-render the titles of the todo item.
   render: function() {
-    console.log(this.model.attributes);
-    console.log(this.$el);
-    this.$el.html(this.todoTpl(this.model.attributes));
+    this.$el.html(this.template(this.model.toJSON()));
     return this;
   }
 
 });
 
 var todoView = new TodoView({model: myTodo});
-
-todoView.render();
+$('#todos').append(todoView.$el);

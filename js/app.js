5566301a7fbd894f5d7c9a30abc31ee09bd63893
pcrglennon <pcrglennon@gmail.com>
2014-09-13 14:15:20 -0400

    Begin refactoring w/ require.js

---
+++ js/app.js
@@ -0,0 +1,76 @@
+
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  var initialize = function() {
+    var Todo = Backbone.Model.extend({
+
+      defaults: {
+        description: '',
+        completed: false
+      }
+    });
+
+    var TodoList = Backbone.Collection.extend({
+      model: Todo
+    });
+
+
+    // Views
+
+
+    var TodoView = Backbone.View.extend({
+
+      tagName: 'li',
+
+      // Cache the template function for a single item.
+      template: _.template( $('#todo-template').html() ),
+
+      // Called when the view is first created
+      initialize: function() {
+        console.log("Todo View init > " + this.model.attributes);
+        this.model.on('change', this.render, this);
+      },
+
+      render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+      }
+
+    });
+
+    var TodoListView = Backbone.View.extend({
+
+      tagName: 'ul',
+      id: 'todos',
+
+      initialize: function() {
+        this.collection.on('add', this.addOne, this);
+      },
+
+      render: function() {
+        this.collection.forEach(this.addOne, this);
+        return this;
+      },
+
+      addOne: function(todo) {
+        var todoView = new TodoView({model: todo});
+        this.$el.append(todoView.render().el);
+      }
+    });
+
+    var todo =         new Todo({description: "Get a job"}),
+    todoList =     new TodoList([todo]),
+    todoListView = new TodoListView({collection: todoList});
+
+    $('#container').append(todoListView.render().el);
+  };
+
+  return {
+    initialize: initialize
+  };
+});
+
+

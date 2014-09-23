83e8e581f7b423e266eb06b58e8161c285662bec
pcrglennon <pcrglennon@gmail.com>
2014-09-23 17:05:36 -0400

    Add jQuery version name to files, add full versions of all JS files + source maps

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 require.config({
   paths: {
-    jquery: 'libs/jquery/jquery.min',
+    jquery: 'libs/jquery/jquery-2.1.1.min',
     underscore: 'libs/underscore/underscore.min',
     backbone: 'libs/backbone/backbone.min',
     text: 'libs/require/text'
